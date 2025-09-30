4d583368d967cb4324cf88edcb9406ba0e4d04f1
Mohammad Rafiq <rafiq@rrv.sh>
2025-09-30 21:59:23 +0800

    build(flake): init w/ flake-parts and import-tree

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  outputs =
+    { self, ... }@inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      (inputs.import-tree ./nix)
+      // {
+        systems = import inputs.systems;
+        flake = {
+          inherit self;
+          paths.root = ./.;
+        };
+      }
+    );
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    import-tree.url = "github:vic/import-tree";
+  };
+}
