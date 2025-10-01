9e1c7dbc955be570ec7a40db2b55366745be3ea3
rafiq <rafiq@rrv.sh>
2025-10-01 23:51:20 +0800

    feat(veil): add working web server (#22)
    * feat(nixos): add nginx to veil
    
    * fix(nixos): let wheel users use sudo no password
    
    * fix(nixos): dont create the file on entry
    
    * fix(nixos): allow http(s) traffic for veil

--- nix/veil.nix
+++ nix/veil.nix
@@ -9,20 +9,33 @@
   flake.nixosConfigurations.veil = lib.nixosSystem {
     modules = [
       "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-      {
-        image.fileName = "nixos-25-11-aarch64-veil.img";
-        sdImage.compressImage = false;
-        nixpkgs.hostPlatform.system = "aarch64-linux";
-        system.stateVersion = "25.11";
-        services.openssh.enable = true;
-        users.users.rafiq = {
-          isNormalUser = true;
-          extraGroups = [ "wheel" ];
-          openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdsZyY3gu8IGB8MzMnLdh+ClDxQQ2RYG9rkeetIKq8n rafiq"
+      (
+        { pkgs, ... }:
+        {
+          image.fileName = "nixos-25-11-aarch64-veil.img";
+          sdImage.compressImage = false;
+          nixpkgs.hostPlatform.system = "aarch64-linux";
+          system.stateVersion = "25.11";
+          services = {
+            openssh.enable = true;
+            nginx.enable = true;
+            nginx.virtualHosts."_".root = "/var/www/veil";
+          };
+          users.users.rafiq = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" ];
+            openssh.authorizedKeys.keys = [
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdsZyY3gu8IGB8MzMnLdh+ClDxQQ2RYG9rkeetIKq8n rafiq"
+            ];
+          };
+          security.sudo.wheelNeedsPassword = false;
+          networking.hostName = "veil";
+          networking.firewall.allowedTCPPorts = [
+            80
+            443
           ];
-        };
-      }
+        }
+      )
     ];
   };
 }
