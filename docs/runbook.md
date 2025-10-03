87f5260e801d62dfbd32f31dd4b48b2bc0650265
rafiq <rafiq@rrv.sh>
2025-10-03 21:39:06 +0800

    feat(nixos): add declarative password management (#30)
    * feat(nix): add nix-community binary cache
    
    * build(nix): init sops
    
    * docs: add sops

---
+++ docs/runbook.md
@@ -0,0 +1,1 @@
+ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/.config/sops/age/keys.txt
--- nix/veil.nix
+++ nix/veil.nix
@@ -9,25 +9,36 @@
   flake.nixosConfigurations.veil = lib.nixosSystem {
     modules = [
       "${inputs.nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+      inputs.sops-nix.nixosModules.sops
       (
-        { pkgs, ... }:
+        { pkgs, config, ... }:
         {
           image.fileName = "nixos-25-11-aarch64-veil.img";
           sdImage.compressImage = false;
           nixpkgs.hostPlatform.system = "aarch64-linux";
           system.stateVersion = "25.11";
+          nix.settings = {
+            substituters = [ "https://nix-community.cachix.org" ];
+            trusted-substituters = [ "https://nix-community.cachix.org" ];
+            trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+          };
           services = {
             openssh.enable = true;
             nginx.enable = true;
             nginx.virtualHosts."_".root = ../www/rrv.sh;
           };
-          users.users.rafiq = {
-            isNormalUser = true;
-            extraGroups = [ "wheel" ];
-            packages = with pkgs; [ git ];
-            openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdsZyY3gu8IGB8MzMnLdh+ClDxQQ2RYG9rkeetIKq8n rafiq"
-            ];
+          users = {
+            mutableUsers = false;
+            groups.users.gid = 100;
+            users.rafiq = {
+              isNormalUser = true;
+              extraGroups = [ "wheel" ];
+              packages = with pkgs; [ git ];
+              hashedPasswordFile = config.sops.secrets."rafiq/hashedPassword".path;
+              openssh.authorizedKeys.keys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdsZyY3gu8IGB8MzMnLdh+ClDxQQ2RYG9rkeetIKq8n rafiq"
+              ];
+            };
           };
           security.sudo.wheelNeedsPassword = false;
           networking.hostName = "veil";
@@ -35,6 +46,13 @@
             80
             443
           ];
+          sops = {
+            age.sshKeyPaths = [ "/home/rafiq/.ssh/id_ed25519" ];
+            secrets."rafiq/hashedPassword" = {
+              neededForUsers = true;
+              sopsFile = ./users.yaml;
+            };
+          };
         }
       )
     ];
