90b038acd5c0430a5830291178ee40661b3e4290
Mohammad Rafiq <rafiq@rrv.sh>
2025-10-05 23:04:18 +0800

    feat(nixos): enable experimental features flakes and nix-command

--- nix/veil.nix
+++ nix/veil.nix
@@ -18,6 +18,7 @@
           nixpkgs.hostPlatform.system = "aarch64-linux";
           system.stateVersion = "25.11";
           nix.settings = {
+            experimental-features = [ "nix-command" "flakes" ];
             substituters = [ "https://nix-community.cachix.org" ];
             trusted-substituters = [ "https://nix-community.cachix.org" ];
             trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
