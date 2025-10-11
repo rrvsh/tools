53564be091797ad8e0b07bf2a913be4ea726e4d8
rafiq <rafiq@rrv.sh>
2025-10-11 14:07:46 +0800

    refactor(nix): decompose veil into modules (#53)
    * refactor(nix): move sops to nixos module
    
    * refactor(nix): move nix config to nixos module
    
    * refactor(nix): move webserver and acme to nixos module
    
    * refactor(nix): move user config to nixos module
    
    * refactor(nix): move networking config to nixos module
    
    * refactor(nix): move platform config to nixos module
    
    * refactor(nix): rename to manifest
    
    * fix(nix): amend paths to fit the new folder structure

---
+++ nix/manifest.nix
@@ -0,0 +1,18 @@
+{ config, ... }:
+let
+  cfg = config.flake;
+in
+{
+  flake.manifest = {
+    hosts.nixos = {
+      veil = {
+        arch = "aarch64";
+        createImage = true;
+        modules = with cfg.modules.nixos; [
+          reverse-proxy
+          rrv-sh
+        ];
+      };
+    };
+  };
+}
--- nix/modules/flake-parts/manifest.nix
+++ nix/modules/flake-parts/manifest.nix
@@ -57,6 +57,10 @@ let
         };
         modules = [
           cfg.modules.nixos.default
+          cfg.modules.nixos.secrets
+          cfg.modules.nixos.nix-config
+          cfg.modules.nixos.users
+          cfg.modules.nixos.networking
         ]
         ++ (optional (hasAttr name cfg.modules.nixos) cfg.modules.nixos.${name})
         ++ (optional value.createImage cfg.modules.nixos.sd-image)
