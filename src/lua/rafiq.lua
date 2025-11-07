bd9fadf7375c5ff8f02f66c15f9ebaed97962d47
Mohammad Rafiq <rafiq@rrv.sh>
2025-11-07 20:58:18 +0800

    add copy to clipboard shortcut in vim

--- src/lua/rafiq.lua
+++ src/lua/rafiq.lua
@@ -5,3 +5,4 @@ vim.o.smarttab = true -- start of line uses shiftwidth, else tabstop
 vim.o.tabstop = 2 -- amount of spaces a tab character represents
 vim.o.softtabstop = 2 -- amount of spaces tab should indent
 vim.o.shiftwidth = 2 -- amount of spaces builtins use by default
+vim.keymap.set('v', 'Y', '"+y') -- copy to clipboard on shift y
