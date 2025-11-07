4710d80d320a2db4f0efc5898ee947cfc24abeae
Mohammad Rafiq <rafiq@rrv.sh>
2025-11-07 13:56:36 +0800

    minor darwin improvements

--- ops/nix/homes/rafiq.nix
+++ ops/nix/homes/rafiq.nix
@@ -1,14 +1,27 @@
 { inputs, lib, ... }:
 {
-  flake.modules.darwin.rafiq = {
-    nixpkgs.overlays = [ inputs.nixpkgs-firefox-darwin.overlay ];
-    nixpkgs.config.allowUnfreePredicate =
-      pkg:
-      builtins.elem (lib.getName pkg) [
-        "firefox-bin"
-      ];
-    system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
-  };
+  flake.modules.darwin.rafiq =
+    { config, ... }:
+    {
+      nixpkgs.overlays = [ inputs.nixpkgs-firefox-darwin.overlay ];
+      nixpkgs.config.allowUnfreePredicate =
+        pkg:
+        builtins.elem (lib.getName pkg) [
+          "firefox-bin"
+        ];
+      system = {
+        activationScripts = {
+          extraActivation.text = lib.mkAfter config.system.activationScripts.pmset.text;
+          pmset.text = ''
+            echo >&2 "configuring power management..."
+            sudo pmset -a disablesleep 1
+          '';
+        };
+        defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
+        keyboard.enableKeyMapping = true;
+        keyboard.remapCapsLockToEscape = true;
+      };
+    };
   flake.modules.homeManager.rafiq =
     { pkgs, ... }:
     {
