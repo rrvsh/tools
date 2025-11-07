b73d83af0505d68c6da11671016ec8cf4d995b06
Mohammad Rafiq <rafiq@rrv.sh>
2025-11-07 20:38:00 +0800

    feat: some vim options

--- src/lua/rafiq.lua
+++ src/lua/rafiq.lua
@@ -1,2 +1,7 @@
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.o.expandtab = true -- insert tabs with spaces
+vim.o.smarttab = true -- start of line uses shiftwidth, else tabstop
+vim.o.tabstop = 2 -- amount of spaces a tab character represents
+vim.o.softtabstop = 2 -- amount of spaces tab should indent
+vim.o.shiftwidth = 2 -- amount of spaces builtins use by default
