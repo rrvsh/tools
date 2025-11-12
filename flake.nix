74987d10f82bd127bc14e4920c1f0805296a4478
Mohammad Rafiq <rafiq@rrv.sh>
2025-11-12 19:31:41 +0800

    build: nixos-generators

--- flake.nix
+++ flake.nix
@@ -34,6 +34,10 @@
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
     yazi.url = "github:sxyazi/yazi";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-rosetta-builder = {
       url = "github:cpick/nix-rosetta-builder";
       inputs.nixpkgs.follows = "nixpkgs";
