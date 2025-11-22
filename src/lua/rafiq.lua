6f7c925e962b973b0785abe4a69b46aab619cd2d
Mohammad Rafiq <rafiq@rrv.sh>
2025-11-22 08:57:57 +0800

    chore: bump flake inputs

--- src/lua/rafiq.lua
+++ src/lua/rafiq.lua
@@ -72,14 +72,14 @@ vim.diagnostic.config({ virtual_text = { current_line = true } })
 vim.lsp.enable("pyright")
 vim.lsp.enable("nil_ls")
 vim.lsp.enable("rust_analyzer")
-vim.lsp.config('rust_analyzer', {
-  settings = {
-    ['rust-analyzer'] = {
-      check = {
-        command = "clippy";
-      }
-    }
-  }
+vim.lsp.config("rust_analyzer", {
+	settings = {
+		["rust-analyzer"] = {
+			check = {
+				command = "clippy",
+			},
+		},
+	},
 })
 vim.lsp.enable("stylua")
 vim.lsp.enable("lua_ls")
