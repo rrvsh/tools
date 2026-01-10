2f64e957e17b241e18dd27d969b8e08247cd742a
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-10 15:27:16 +0800

    feat(tf): add needed stuff for gha

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,78 @@
+variable "project_id" {
+  type    = string
+  default = "rrvsh-production"
+}
+
 provider "google" {
-  project = "rrvsh"
+  project = var.project_id
   region = "asia-southeast1"
   zone = "asia-southeast1-a"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "rrvsh-tools-production-tfstate"
+    prefix  = "terraform/state"
+  }
+}
+
+resource "google_project_service" "iam_api" {
+  service = "iam.googleapis.com"
+}
+
+resource "google_service_account" "infra_ci_sa" {
+  account_id = "infra-ci"
+  description = "Used for running OpenTofu commands in CI."
+}
+
+resource "google_iam_workload_identity_pool" "gha_pool" {
+  workload_identity_pool_id = "github-actions"
+}
+
+resource "google_iam_workload_identity_pool_provider" "gha_tools_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.gha_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "tools"
+  attribute_mapping = {
+    "google.subject"              = "assertion.sub"
+    "attribute.actor"             = "assertion.actor"
+    "attribute.repository"        = "assertion.repository"
+    "attribute.repository_owner"  = "assertion.repository_owner"
+  }
+  attribute_condition = "assertion.repository == 'rrvsh/tools'"
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
+}
+
+resource "google_service_account_iam_member" "gha_impersonation" {
+  service_account_id = google_service_account.infra_ci_sa.name
+  member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.gha_pool.name}/attribute.repository/rrvsh/tools"
+  role               = "roles/iam.workloadIdentityUser"
+}
+
+resource "google_service_account_iam_member" "rafiq_impersonation" {
+  service_account_id = google_service_account.infra_ci_sa.name
+  member             = "user:rafiq@rrv.sh"
+  role               = "roles/iam.serviceAccountTokenCreator"
+}
+
+resource "google_storage_bucket" "tfstate" {
+  name     = "rrvsh-tools-production-tfstate"
+  location = "ASIA-SOUTHEAST1"
+  uniform_bucket_level_access = true
+  force_destroy               = false
+}
+
+resource "google_storage_bucket_iam_member" "tfstate_ci" {
+  bucket = google_storage_bucket.tfstate.name
+  member = "serviceAccount:${google_service_account.infra_ci_sa.email}"
+  role   = "roles/storage.objectAdmin"
+}
+
+output "gha_provider_name" {
+  value = google_iam_workload_identity_pool_provider.gha_tools_provider.name
+}
+
+output "infra_ci_sa_email" {
+  value = google_service_account.infra_ci_sa.email
 }
