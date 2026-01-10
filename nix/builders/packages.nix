91bea658ada4566d9eaae8d5332a84ec1dd3ae41
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-10 11:43:11 +0800

    build(nix): package rrvsh as oci image

--- nix/builders/packages.nix
+++ nix/builders/packages.nix
@@ -1,16 +1,24 @@
 { config, ... }:
 let
+  name = "rrvsh";
   src = config.flake.paths.root + /rs;
   cargoLock.lockFile = src + /Cargo.lock;
 in
 {
   perSystem =
-    { pkgs, ... }:
+    { pkgs, self', ... }:
     {
-      packages.rrvsh = pkgs.rustPlatform.buildRustPackage {
-        inherit src cargoLock;
-        name = "rrvsh";
-        cargoBuildFlags = [ "--package rrvsh" ];
+      packages = {
+        rrvsh-bin = pkgs.rustPlatform.buildRustPackage {
+          inherit cargoLock name src;
+          cargoBuildFlags = [ "--package ${name}" ];
+        };
+        rrvsh-image = pkgs.dockerTools.buildLayeredImage {
+          inherit name;
+          tag = "latest";
+          contents = [ self'.packages.rrvsh-bin ];
+          config.Cmd = "/bin/rrvsh";
+        };
       };
     };
 }
