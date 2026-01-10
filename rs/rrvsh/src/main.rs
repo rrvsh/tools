b968af99d6a199ee6472c50e3f910e49894c0be9
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-10 08:31:23 +0800

    build(rs): initialise askama

--- rs/rrvsh/src/main.rs
+++ rs/rrvsh/src/main.rs
@@ -1,11 +1,37 @@
-use axum::{Router, routing::get};
+use askama::Template;
+use axum::{
+    Router,
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+};
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate {
+    name: String,
+}
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let host = "0.0.0.0";
+    let port = 3000;
+    let addr = format!("{host}:{port}");
+    let listener = tokio::net::TcpListener::bind(addr)
+        .await
+        .expect("Error binding to port {port}");
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let app = Router::new().route("/hello", get(hello_get));
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_get() -> Result<Response, StatusCode> {
+    HelloTemplate {
+        name: "world".to_string(),
+    }
+    .render()
+    .map_or_else(
+        |_| Err(StatusCode::INTERNAL_SERVER_ERROR),
+        |rendered| Ok(Html(rendered).into_response()),
+    )
 }
