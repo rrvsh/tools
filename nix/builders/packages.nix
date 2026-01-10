76cf5420c1014744f397fac9184334cd82e28e98
rafiq <rafiq@rrv.sh>
2026-01-10 11:54:30 +0800

    build(nix): package rrvsh as container image (#121)
    Adds nix/builders/packages.nix which packages the rrvsh binary and OCI container image as Nix derivations, along with helpers to run and remove the container.

---
+++ nix/builders/packages.nix
@@ -0,0 +1,24 @@
+{ config, ... }:
+let
+  name = "rrvsh";
+  src = config.flake.paths.root + /rs;
+  cargoLock.lockFile = src + /Cargo.lock;
+in
+{
+  perSystem =
+    { pkgs, self', ... }:
+    {
+      packages = {
+        rrvsh-bin = pkgs.rustPlatform.buildRustPackage {
+          inherit cargoLock name src;
+          cargoBuildFlags = [ "--package ${name}" ];
+        };
+        rrvsh-image = pkgs.dockerTools.buildLayeredImage {
+          inherit name;
+          tag = "latest";
+          contents = [ self'.packages.rrvsh-bin ];
+          config.Cmd = "/bin/rrvsh";
+        };
+      };
+    };
+}
