4c62d4db02caed98cbb4d6bf1da990b731c5584b
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-10 19:10:00 +0800

    build(tf): init cloudflare

--- tf/main.tf
+++ tf/main.tf
@@ -3,10 +3,20 @@ terraform {
     bucket  = "rrvsh-tools-production-tfstate"
     prefix  = "terraform/state"
   }
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
 }
 
 provider "google" {
   project = var.project_id
   region = "asia-southeast1"
   zone = "asia-southeast1-a"
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
--- tf/variables.tf
+++ tf/variables.tf
@@ -2,3 +2,7 @@ variable "project_id" {
   type    = string
   default = "rrvsh-production"
 }
+
+variable "cloudflare_api_token" {
+  type    = string
+}
