bd211de2fc365a51582271edcb6892330676b08c
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-10 08:04:22 +0800

    refactor(nix): move devShell under builders

--- nix/builders/devShells.nix
+++ nix/builders/devShells.nix
@@ -5,12 +5,7 @@
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           # general
-          bacon
-          caligula
           just
-          sops
-          ssh-to-age
-          zstd
 
           # nix
           deadnix
