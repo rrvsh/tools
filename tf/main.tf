6b520b40e5921f1305115dc6b69ff34cd944516b
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-10 15:34:31 +0800

    fix(tf): give infra ci sa needed perms

--- tf/main.tf
+++ tf/main.tf
@@ -29,6 +29,36 @@ resource "google_service_account" "infra_ci_sa" {
   description = "Used for running OpenTofu commands in CI."
 }
 
+resource "google_project_iam_member" "infra_ci_sa_admin" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountAdmin"
+  member  = "serviceAccount:${google_service_account.infra_ci_sa.email}"
+}
+
+resource "google_project_iam_member" "infra_ci_wif_admin" {
+  project = var.project_id
+  role    = "roles/iam.workloadIdentityPoolAdmin"
+  member  = "serviceAccount:${google_service_account.infra_ci_sa.email}"
+}
+
+resource "google_project_iam_member" "infra_ci_security_admin" {
+  project = var.project_id
+  role    = "roles/iam.securityAdmin"
+  member  = "serviceAccount:${google_service_account.infra_ci_sa.email}"
+}
+
+resource "google_project_iam_member" "infra_ci_serviceusage_admin" {
+  project = var.project_id
+  role    = "roles/serviceusage.serviceUsageAdmin"
+  member  = "serviceAccount:${google_service_account.infra_ci_sa.email}"
+}
+
+resource "google_storage_bucket_iam_member" "infra_ci_tfstate_admin" {
+  bucket = google_storage_bucket.tfstate.name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.infra_ci_sa.email}"
+}
+
 resource "google_iam_workload_identity_pool" "gha_pool" {
   workload_identity_pool_id = "github-actions"
 }
