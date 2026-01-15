adfb27aa9873b27a8489bef1ed9608d1a3075d9b
rafiq <rafiq@rrv.sh>
2026-01-15 21:20:56 +0800

    refactor(site): move app into module and set up index and blog routes (#139)

--- rs/site/src/app/routes.rs
+++ rs/site/src/app/routes.rs
@@ -2,20 +2,32 @@ use askama::Template;
 use axum::{
     http::StatusCode,
     response::{Html, IntoResponse, Response},
+    routing::get,
 };
 
-#[derive(Template)]
-#[template(path = "hello.html")]
-struct HelloTemplate {
-    name: String,
+pub fn build_router() -> axum::Router {
+    axum::Router::new()
+        .route("/", get(index_get))
+        .route("/blog", get(blog_index_get))
 }
 
-pub async fn hello() -> Result<Response, StatusCode> {
-    HelloTemplate {
-        name: "world".to_string(),
-    }
-    .render()
-    .map_or_else(
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {}
+
+pub async fn index_get() -> Result<Response, StatusCode> {
+    IndexTemplate {}.render().map_or_else(
+        |_| Err(StatusCode::INTERNAL_SERVER_ERROR),
+        |rendered| Ok(Html(rendered).into_response()),
+    )
+}
+
+#[derive(Template)]
+#[template(path = "blog/index.html")]
+struct BlogIndexTemplate {}
+
+pub async fn blog_index_get() -> Result<Response, StatusCode> {
+    BlogIndexTemplate {}.render().map_or_else(
         |_| Err(StatusCode::INTERNAL_SERVER_ERROR),
         |rendered| Ok(Html(rendered).into_response()),
     )
