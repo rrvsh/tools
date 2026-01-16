e9c250ddcae6a35e9276092b6f6a2ce396fa061b
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-16 19:31:49 +0800

    feat(site): stub article route

--- rs/site/src/app/routes.rs
+++ rs/site/src/app/routes.rs
@@ -1,5 +1,6 @@
 use askama::Template;
 use axum::{
+    extract::Path,
     http::StatusCode,
     response::{Html, IntoResponse, Response},
     routing::get,
@@ -9,6 +10,7 @@ pub fn build_router() -> axum::Router {
     axum::Router::new()
         .route("/", get(index_get))
         .route("/blog", get(blog_index_get))
+        .route("/{year}/{month}/{day}/{slug}", get(article_get))
 }
 
 #[derive(Template)]
@@ -28,6 +30,22 @@ struct BlogIndexTemplate {}
 
 pub async fn blog_index_get() -> Result<Response, StatusCode> {
     BlogIndexTemplate {}.render().map_or_else(
+        |_| Err(StatusCode::INTERNAL_SERVER_ERROR),
+        |rendered| Ok(Html(rendered).into_response()),
+    )
+}
+
+#[derive(Template)]
+#[template(path = "article.html")]
+struct ArticleTemplate {
+    content: String,
+}
+
+pub async fn article_get(
+    Path((year, month, day, slug)): Path<(i32, i32, i32, String)>,
+) -> Result<Response, StatusCode> {
+    let content = format!("{year}-{month}-{day}-{slug}");
+    ArticleTemplate { content }.render().map_or_else(
         |_| Err(StatusCode::INTERNAL_SERVER_ERROR),
         |rendered| Ok(Html(rendered).into_response()),
     )
