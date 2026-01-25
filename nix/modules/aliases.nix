9fe69d27412ed0fd87dd6b724fc184b6bb4878ea
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-25 21:11:57 +0800

    feat(notes): add in out and projects

--- nix/modules/aliases.nix
+++ nix/modules/aliases.nix
@@ -4,6 +4,9 @@
       home.shellAliases = {
         v = "$EDITOR";
         e = "fish -c 'set -e var; set var (sk); test -n \"$var\"; and $EDITOR $var'";
+        "in" = "mkdir -p ~/ref && $EDITOR ~/ref/inbox.md";
+        out = "mkdir -p ~/ref && $EDITOR ~/ref/out.md";
+        projects = "mkdir -p ~/ref/projects && $EDITOR ~/ref/projects/projects.md";
       };
     };
   };
