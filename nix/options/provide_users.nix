056946afffe9b015837ab0fb6ca80fd637fe72fe
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-26 14:32:57 +0800

    move admin def into provide users

--- nix/options/provide_users.nix
+++ nix/options/provide_users.nix
@@ -1,5 +1,7 @@
-{ lib, ... }:
+{ lib, config, ... }:
 let
+  cfg = config.flake;
+  inherit (lib.lists) elemAt;
   inherit (lib.options) mkOption;
   inherit (lib.types)
     str
@@ -7,26 +9,40 @@ let
     submodule
     bool
     ;
+  primaryUsers = lib.attrsets.filterAttrs (_: value: value.primary or false) cfg.users.users;
 in
 {
-  options.flake.users.users = mkOption {
-    type = attrsOf (submodule {
-      options = {
-        primary = mkOption {
-          type = bool;
-          default = false;
-          description = "Denote this user as able to modify the systems controlled by this flake.";
+  options.flake.users = {
+    users = mkOption {
+      type = attrsOf (submodule {
+        options = {
+          primary = mkOption {
+            type = bool;
+            default = false;
+            description = "Denote this user as able to modify the systems controlled by this flake.";
+          };
+          email = mkOption {
+            # NOTE: this is not used for anything now but will be for cloudflare etc
+            type = str;
+            description = "The email of the user.";
+          };
+          pubkey = mkOption {
+            type = str;
+            description = "The SSH public key of the user. Used to add the user as authorised on every machine.";
+          };
         };
-        email = mkOption {
-          # NOTE: this is not used for anything now but will be for cloudflare etc
-          type = str;
-          description = "The email of the user.";
-        };
-        pubkey = mkOption {
-          type = str;
-          description = "The SSH public key of the user. Used to add the user as authorised on every machine.";
-        };
-      };
-    });
+      });
+    };
+    admin.username = mkOption {
+      internal = true;
+      type = str;
+    };
+  };
+  config.flake = {
+    users.admin.username = elemAt (builtins.attrNames primaryUsers) 0;
+    modules.darwin.default = {
+      system.primaryUser = cfg.users.admin.username;
+      nix.settings.trusted-users = [ cfg.users.admin.username ];
+    };
   };
 }
