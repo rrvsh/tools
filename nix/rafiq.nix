4d81d7b9f777221686e10d606116b7672771c714
Mohammad Rafiq <rafiq@rrv.sh>
2026-01-26 10:48:45 +0800

    chore(aliases): make projects pro

--- nix/rafiq.nix
+++ nix/rafiq.nix
@@ -11,7 +11,7 @@
         e = "fish -c 'set -e var; set var (sk); test -n \"$var\"; and $EDITOR $var'";
         "in" = "mkdir -p ~/ref && $EDITOR ~/ref/inbox.md";
         out = "mkdir -p ~/ref && $EDITOR ~/ref/out.md";
-        projects = "mkdir -p ~/ref/projects && $EDITOR ~/ref/projects/projects.md";
+        pro = "mkdir -p ~/ref/projects && $EDITOR ~/ref/projects/projects.md";
       };
     };
   };
