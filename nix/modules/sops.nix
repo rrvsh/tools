406a62d37fd4fd3c905f7b0f06fd045201719767
rafiq <rafiq@rrv.sh>
2026-01-31 10:11:09 +0800

    feat(nix): set rafiq password with sops (#157)

---
+++ nix/modules/sops.nix
@@ -0,0 +1,23 @@
+{ inputs, ... }:
+{
+  config.flake = {
+    modules.nixos.default =
+      { config, ... }:
+      let
+        homeDirectory = config.users.users.rafiq.home;
+      in
+      {
+        imports = [ inputs.sops-nix.nixosModules.sops ];
+        sops.age.sshKeyPaths = [ "${homeDirectory}/.ssh/id_ed25519" ];
+      };
+    modules.darwin.default =
+      { config, ... }:
+      let
+        homeDirectory = config.users.users.rafiq.home;
+      in
+      {
+        imports = [ inputs.sops-nix.darwinModules.sops ];
+        sops.age.sshKeyPaths = [ "${homeDirectory}/.ssh/id_ed25519" ];
+      };
+  };
+}
