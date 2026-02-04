dec0f371d2ae93fbd5c4eec6f16947b2f6cb984e
Mohammad Rafiq <rafiq@rrv.sh>
2026-02-04 11:14:47 +0800

    chore(ci): split checks into parallel jobs
    Use smaller CI devShells to cut setup time.

--- nix/outputs/devShells.nix
+++ nix/outputs/devShells.nix
@@ -2,42 +2,74 @@
   perSystem =
     { pkgs, ... }:
     {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          # general
-          just
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # general
+            just
 
-          # nix
-          deadnix
-          nh
-          nixfmt-tree
-          statix
+            # nix
+            deadnix
+            nh
+            nixfmt-tree
+            statix
 
-          # lua
-          luajitPackages.luacheck
-          stylua
+            # lua
+            luajitPackages.luacheck
+            stylua
 
-          # gha yaml
-          gh
-          zizmor
+            # gha yaml
+            gh
+            zizmor
 
-          # rs
-          cargo
-          clippy
-          rustc
-          rustfmt
-          bacon
+            # rs
+            cargo
+            clippy
+            rustc
+            rustfmt
+            bacon
 
-          # docker
-          docker
-          colima
+            # docker
+            docker
+            colima
 
-          # aws
-          awscli2
+            # aws
+            awscli2
 
-          # terraform
-          opentofu
-        ];
+            # terraform
+            opentofu
+          ];
+        };
+
+        ci-gha = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gh
+            zizmor
+          ];
+        };
+
+        ci-lua = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            stylua
+          ];
+        };
+
+        ci-nix = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            deadnix
+            nixfmt-tree
+            statix
+          ];
+        };
+
+        ci-rs = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            clippy
+            rustc
+            rustfmt
+          ];
+        };
       };
     };
 }
