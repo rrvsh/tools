0c8497857c3fdb5b576dd700836db9e094b2a8cb
Mohammad Rafiq <rafiq@rrv.sh>
2026-02-04 11:22:24 +0800

    chore(ci): use just for workflow steps
    Keep CI logic in the Justfile and dedupe devShell inputs.

--- nix/outputs/devShells.nix
+++ nix/outputs/devShells.nix
@@ -1,74 +1,83 @@
 {
   perSystem =
     { pkgs, ... }:
+    let
+      ciInputs = {
+        gha = with pkgs; [
+          gh
+          zizmor
+        ];
+        lua = with pkgs; [
+          stylua
+        ];
+        nix = with pkgs; [
+          deadnix
+          nixfmt-tree
+          statix
+        ];
+        rs = with pkgs; [
+          cargo
+          clippy
+          rustc
+          rustfmt
+        ];
+      };
+      defaultInputs = with pkgs; [
+        # general
+        just
+
+        # nix
+        deadnix
+        nh
+        nixfmt-tree
+        statix
+
+        # lua
+        luajitPackages.luacheck
+        stylua
+
+        # gha yaml
+        gh
+        zizmor
+
+        # rs
+        cargo
+        clippy
+        rustc
+        rustfmt
+        bacon
+
+        # docker
+        docker
+        colima
+
+        # aws
+        awscli2
+
+        # terraform
+        opentofu
+      ];
+    in
     {
       devShells = {
         default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            # general
-            just
-
-            # nix
-            deadnix
-            nh
-            nixfmt-tree
-            statix
-
-            # lua
-            luajitPackages.luacheck
-            stylua
-
-            # gha yaml
-            gh
-            zizmor
-
-            # rs
-            cargo
-            clippy
-            rustc
-            rustfmt
-            bacon
-
-            # docker
-            docker
-            colima
-
-            # aws
-            awscli2
-
-            # terraform
-            opentofu
-          ];
+          buildInputs = defaultInputs;
         };
 
         ci-gha = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            gh
-            zizmor
-          ];
+          buildInputs = ciInputs.gha;
         };
 
         ci-lua = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            stylua
-          ];
+          buildInputs = ciInputs.lua;
         };
 
         ci-nix = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            deadnix
-            nixfmt-tree
-            statix
-          ];
+          buildInputs = ciInputs.nix;
         };
 
         ci-rs = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cargo
-            clippy
-            rustc
-            rustfmt
-          ];
+          buildInputs = ciInputs.rs;
         };
       };
     };
