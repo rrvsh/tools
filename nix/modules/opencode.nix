fbaf9d8bdfdb7b9207845c2a8cc2cf33b4275657
Lumen <lumen@rrvsh>
2026-02-05 12:03:28 +0800

    feat(nix): allow opencode temp worktrees

--- nix/modules/opencode.nix
+++ nix/modules/opencode.nix
@@ -1,8 +1,18 @@
 {
   config.flake = {
     modules.homeManager.rafiq = {
-      programs.opencode.enable = true;
-      programs.opencode.enableMcpIntegration = true;
+      programs = {
+        opencode = {
+          enable = true;
+          enableMcpIntegration = true;
+          settings = {
+            permission.external_directory = {
+              "/tmp/**" = "allow";
+              "/var/folders/**" = "allow";
+            };
+          };
+        };
+      };
       home.shellAliases.oc = "opencode";
     };
   };
