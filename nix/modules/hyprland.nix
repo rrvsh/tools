98e7d74dc407a9e36c0c73cf5221f42727572e07
Mohammad Rafiq <rafiq@rrv.sh>
2026-02-18 08:39:41 +0800

    feat(hyprland): add window management keybinds

--- nix/modules/hyprland.nix
+++ nix/modules/hyprland.nix
@@ -40,10 +40,27 @@
               ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
               ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
               ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-              "CTRL_SHIFT, 1, exec, ghostty"
-              "CTRL_SHIFT, 2, exec, firefox"
-              "CTRL_SHIFT, 3, exec, steam"
-              "CTRL_SHIFT, 4, exec, obs"
+              # ctrl alt for window opening/closing
+              "ALT_CTRL, 1, exec, ghostty"
+              "ALT_CTRL, 2, exec, firefox"
+              "ALT_CTRL, 3, exec, steam"
+              "ALT_CTRL, 4, exec, obs"
+              "ALT_CTRL, w, killactive"
+              # alt for selecting
+              "ALT, TAB, cyclenext"
+              "ALT, H, movefocus, l"
+              "ALT, J, movefocus, d"
+              "ALT, K, movefocus, u"
+              "ALT, L, movefocus, r"
+              "ALT_SUPER, H, workspace, -1"
+              "ALT_SUPER, L, workspace, +1"
+              # alt shift for moving
+              "ALT_SHIFT, H, movewindow, l"
+              "ALT_SHIFT, J, movewindow, d"
+              "ALT_SHIFT, K, movewindow, u"
+              "ALT_SHIFT, L, movewindow, r"
+              "ALT_SHIFT_SUPER, H, movetoworkspace, -1"
+              "ALT_SHIFT_SUPER, L, movetoworkspace, +1"
             ];
             # on release
             bindc = [ "ALT_SHIFT, mouse:272, togglefloating" ];
