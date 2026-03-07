3f71f009924c89f42107225ee7be7645e15f832f
Mohammad Rafiq <rafiq@rrv.sh>
2026-03-07 14:14:25 +0800

    Revert "dedupe activation script"
    This reverts commit 30dcb5b49a0a3f26fc655edc7e6df25d5ec52628.

--- nix/modules/services/builders-remote.nix
+++ nix/modules/services/builders-remote.nix
@@ -4,21 +4,19 @@ let
   inherit (cfg.accounts.rafiq) username;
   nixosRootSshKey = "/root/.ssh/id_ed25519";
   darwinRootSshKey = "/var/root/.ssh/id_ed25519";
-  mkActivationScript = from: to: ''
-    echo >&2 "copying ssh key to root for remote builders..."
-    mkdir -p ${builtins.dirOf to}
-    cp ${from} ${to} 2>/dev/null || true
-    chmod 600 ${to} 2>/dev/null || true
-  '';
 in
 {
   config.flake = {
     modules.nixos.default =
       { config, ... }:
       {
-        system.activationScripts.remote-builder-ssh-key.text = lib.optionalString (
-          config.users.users ? ${username}
-        ) (mkActivationScript "/home/${username}/.ssh/id_ed25519" nixosRootSshKey);
+        system.activationScripts.remote-builder-ssh-key.text =
+          lib.optionalString (config.users.users ? ${username})
+            ''
+              mkdir -p ${builtins.dirOf nixosRootSshKey}
+              cp /home/${username}/.ssh/id_ed25519 ${nixosRootSshKey} 2>/dev/null || true
+              chmod 600 ${nixosRootSshKey} 2>/dev/null || true
+            '';
         nix.buildMachines = [
           {
             hostName = "nemesis";
@@ -52,9 +50,12 @@ in
       };
 
     modules.darwin.default = {
-      system.activationScripts.extraActivation.text = lib.mkAfter (
-        mkActivationScript "/Users/${username}/.ssh/id_ed25519" darwinRootSshKey
-      );
+      system.activationScripts.extraActivation.text = lib.mkAfter ''
+        echo >&2 "copying ssh key to root for remote builders..."
+        sudo mkdir -p ${builtins.dirOf darwinRootSshKey}
+        sudo cp /Users/${username}/.ssh/id_ed25519 ${darwinRootSshKey} 2>/dev/null || true
+        sudo chmod 600 ${darwinRootSshKey} 2>/dev/null || true
+      '';
       nix.buildMachines = [
         {
           hostName = "nemesis";
