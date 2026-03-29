e23d33cb59d1d01ad75ad469637668ba0ff110d7
Mohammad Rafiq <rafiq@rrv.sh>
2026-03-29 12:47:50 +0800

    dedupe devshells

---
+++ nix/AGENTS.md
@@ -0,0 +1,7 @@
+After making nix changes, run these to auto-fix:
+- Run `just format-nix` to format nix files.
+- Run `just lint-nix` to lint and fix nix files.
+
+Run these to check before handing off to user:
+- Run `just check-nix` to run checks.
+- Run `just test-nix` to test nix flakes.
---
+++ nix/devShells.nix
@@ -0,0 +1,75 @@
+{
+  perSystem =
+    { pkgs, ... }:
+    let
+      rustShellHook = ''
+        export CARGO_HOME="$HOME/.cache/tools/cargo"
+        export RUSTUP_HOME="$HOME/.cache/tools/rustup"
+        mkdir -p "$CARGO_HOME" "$RUSTUP_HOME"
+      '';
+      common = with pkgs; [ just ];
+      nixTools = with pkgs; [
+        deadnix
+        nixfmt-tree
+        statix
+      ];
+      rustTools = with pkgs; [
+        cargo
+        clippy
+        rustc
+        rustfmt
+      ];
+      luaTools = with pkgs; [
+        stylua
+        luajitPackages.luacheck
+      ];
+      ghaTools = with pkgs; [
+        gh
+        zizmor
+      ];
+    in
+    {
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = common;
+        };
+
+        ci-nix = pkgs.mkShell {
+          buildInputs = common ++ nixTools;
+        };
+
+        ci-rust = pkgs.mkShell {
+          buildInputs = common ++ rustTools;
+          shellHook = rustShellHook;
+        };
+
+        ci-lua = pkgs.mkShell {
+          buildInputs = common ++ luaTools;
+        };
+
+        ci-gha = pkgs.mkShell {
+          buildInputs = common ++ ghaTools;
+        };
+
+        ci-all = pkgs.mkShell {
+          buildInputs =
+            common
+            ++ nixTools
+            ++ luaTools
+            ++ ghaTools
+            ++ rustTools
+            ++ (with pkgs; [
+              age
+              awscli2
+              bacon
+              colima
+              docker
+              nh
+              opentofu
+              ssh-to-age
+            ]);
+          shellHook = rustShellHook;
+        };
+      };
+    };
+}
