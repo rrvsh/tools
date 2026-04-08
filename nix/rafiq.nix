da8dcea4893d026466ae69a25e13d691371222f4
Mohammad Rafiq <rafiq@rrv.sh>
2026-04-08 12:32:45 +0800

    chore(hyprland): remove borders from windows

--- nix/rafiq.nix
+++ nix/rafiq.nix
@@ -179,6 +179,7 @@ let
           general = {
             gaps_in = 0;
             gaps_out = 0;
+            border_size = 0;
           };
           bind = [
             ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
