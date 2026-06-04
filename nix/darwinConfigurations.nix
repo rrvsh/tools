d286e804e7f545d7e1927732a019501dd3a46504
Mohammad Rafiq <rafiq@rrv.sh>
2026-06-04 17:57:30 +0800

    refactor(hosts): centralize flake host helpers

--- nix/darwinConfigurations.nix
+++ nix/darwinConfigurations.nix
@@ -1,75 +1,16 @@
 {
-  lib,
   inputs,
   config,
+  toolsLib,
   ...
 }:
 let
   cfg = config.flake;
-  profileModule = profile: builtins.getAttr "profile-${profile}" cfg.modules.darwin;
-  primaryUserType = lib.types.submodule {
-    options = {
-      name = lib.mkOption { type = lib.types.str; };
-      fullName = lib.mkOption { type = lib.types.str; };
-      email = lib.mkOption { type = lib.types.str; };
-      gitDefaultBranch = lib.mkOption {
-        type = lib.types.str;
-        default = "main";
-      };
-      sshAuthorizedKeys = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ ];
-      };
-    };
-  };
-  mkDarwinSystem =
-    name: value:
-    inputs.nix-darwin.lib.darwinSystem {
-      specialArgs = {
-        hostName = name;
-        inherit (value) primaryUser;
-      };
-      modules = [
-        (
-          { primaryUser, ... }:
-          {
-            networking.hostName = name;
-            nixpkgs = { inherit (value) hostPlatform; };
-            home-manager.extraSpecialArgs = {
-              hostName = name;
-              inherit primaryUser;
-            };
-          }
-        )
-        cfg.modules.darwin.profile-default
-      ]
-      ++ map profileModule value.profiles
-      ++ value.modules;
-    };
 in
 {
-  options.flake.hosts.darwin = lib.mkOption {
-    type = lib.types.attrsOf (
-      lib.types.submodule {
-        options = {
-          hostPlatform = lib.mkOption {
-            type = lib.types.str;
-          };
-          primaryUser = lib.mkOption {
-            type = primaryUserType;
-          };
-          profiles = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            default = [ ];
-          };
-          modules = lib.mkOption {
-            type = lib.types.listOf lib.types.deferredModule;
-            default = [ ];
-          };
-        };
-      }
-    );
-    default = { };
-  };
-  config.flake.darwinConfigurations = builtins.mapAttrs mkDarwinSystem cfg.hosts.darwin;
+  options.flake.hosts.darwin = toolsLib.hosts.hostOptions;
+  config.flake.darwinConfigurations = builtins.mapAttrs (toolsLib.hosts.mkSystem {
+    systemBuilder = inputs.nix-darwin.lib.darwinSystem;
+    platformModules = cfg.modules.darwin;
+  }) cfg.hosts.darwin;
 }
--- nix/nixosConfigurations.nix
+++ nix/nixosConfigurations.nix
@@ -1,75 +1,16 @@
 {
-  lib,
   inputs,
   config,
+  toolsLib,
   ...
 }:
 let
   cfg = config.flake;
-  profileModule = profile: builtins.getAttr "profile-${profile}" cfg.modules.nixos;
-  primaryUserType = lib.types.submodule {
-    options = {
-      name = lib.mkOption { type = lib.types.str; };
-      fullName = lib.mkOption { type = lib.types.str; };
-      email = lib.mkOption { type = lib.types.str; };
-      gitDefaultBranch = lib.mkOption {
-        type = lib.types.str;
-        default = "main";
-      };
-      sshAuthorizedKeys = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ ];
-      };
-    };
-  };
-  mkNixosSystem =
-    name: value:
-    inputs.nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        hostName = name;
-        inherit (value) primaryUser;
-      };
-      modules = [
-        (
-          { primaryUser, ... }:
-          {
-            networking.hostName = name;
-            nixpkgs = { inherit (value) hostPlatform; };
-            home-manager.extraSpecialArgs = {
-              hostName = name;
-              inherit primaryUser;
-            };
-          }
-        )
-        cfg.modules.nixos.profile-default
-      ]
-      ++ map profileModule value.profiles
-      ++ value.modules;
-    };
 in
 {
-  options.flake.hosts.nixos = lib.mkOption {
-    type = lib.types.attrsOf (
-      lib.types.submodule {
-        options = {
-          hostPlatform = lib.mkOption {
-            type = lib.types.str;
-          };
-          primaryUser = lib.mkOption {
-            type = primaryUserType;
-          };
-          profiles = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            default = [ ];
-          };
-          modules = lib.mkOption {
-            type = lib.types.listOf lib.types.deferredModule;
-            default = [ ];
-          };
-        };
-      }
-    );
-    default = { };
-  };
-  config.flake.nixosConfigurations = builtins.mapAttrs mkNixosSystem cfg.hosts.nixos;
+  options.flake.hosts.nixos = toolsLib.hosts.hostOptions;
+  config.flake.nixosConfigurations = builtins.mapAttrs (toolsLib.hosts.mkSystem {
+    systemBuilder = inputs.nixpkgs.lib.nixosSystem;
+    platformModules = cfg.modules.nixos;
+  }) cfg.hosts.nixos;
 }
