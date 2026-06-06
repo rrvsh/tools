5bbdd588b40f80a2aedd11593406633c742829e6
Mohammad Rafiq <rafiq@rrv.sh>
2026-06-06 16:05:40 +0800

    feat(site.nix): add site deploy package

--- nix/packages/site.nix
+++ nix/packages/site.nix
@@ -5,11 +5,36 @@ in
 {
   perSystem =
     { pkgs, ... }:
-    {
-      packages.site-bin = pkgs.rustPlatform.buildRustPackage {
+    let
+      siteBin = pkgs.rustPlatform.buildRustPackage {
         name = "site";
         src = root + /rs;
         cargoLock.lockFile = root + /rs/Cargo.lock;
+      };
+      siteDeploy = pkgs.stdenvNoCC.mkDerivation {
+        name = "site-deploy";
+        dontUnpack = true;
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/share/rrv-sh
+          cp -R ${root + /rs/site/static} $out/share/rrv-sh/static
+          makeWrapper ${siteBin}/bin/site $out/bin/site \
+            --set STATIC_DIR $out/share/rrv-sh/static \
+            --prefix PATH : ${
+              pkgs.lib.makeBinPath [
+                pkgs.git
+                pkgs.git-lfs
+              ]
+            }
+          runHook postInstall
+        '';
+      };
+    in
+    {
+      packages = {
+        site-bin = siteBin;
+        site-deploy = siteDeploy;
       };
     };
 }
