74226680838d5142eccb361b14d89786e82ccc30
Mohammad Rafiq <rafiq@rrv.sh>
2026-06-08 11:59:48 +0800

    feat(nemesis): add gtnh desktop entry

--- nix/modules/prismlauncher.nix
+++ nix/modules/prismlauncher.nix
@@ -10,8 +10,13 @@ in
     darwin.prismlauncher = osModule;
     nixos.prismlauncher = osModule;
     homeManager.prismlauncher =
-      { pkgs, ... }:
+      { config, pkgs, ... }:
       {
+        xdg.desktopEntries.gtnh = {
+          name = "GregTech New Horizons";
+          icon = "${config.home.homeDirectory}/.local/share/PrismLauncher/instances/GT_New_Horizons_2.8.4_Java_17-25/icon.png";
+          exec = "prismlauncher --launch GT_New_Horizons_2.8.4_Java_17-25";
+        };
         home.packages = [
           (pkgs.prismlauncher.override {
             jdks = [ pkgs.jdk25 ];
