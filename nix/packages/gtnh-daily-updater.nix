939b7a6ad889aa4c645fa027ef63e0d822f3fde6
Mohammad Rafiq <rafiq@rrv.sh>
2026-06-12 20:16:33 +0800

    feat(gtnh-daily-server): add daily GTNH server

--- nix/packages/gtnh-daily-updater.nix
+++ nix/packages/gtnh-daily-updater.nix
@@ -13,7 +13,7 @@
         vendorHash = "sha256-qW5Ybfbo+ss/f9QZv1zTCERhk8cN8I3A3kuPs2eA8rw=";
         nativeCheckInputs = [ pkgs.git ];
         meta = {
-          description = "Automated updater for GT: New Horizons nightly builds";
+          description = "Automated updater for GT: New Horizons daily builds";
           homepage = "https://github.com/Caedis/gtnh-daily-updater";
           mainProgram = "gtnh-daily-updater";
         };
