ef371cca6d8cab53b03fd13e569f44aa62d47d26
Mohammad Rafiq <rafiq@rrv.sh>
2026-06-12 20:16:33 +0800

    feat(gtnh-daily-server): add daily GTNH server

--- nix/modules/prismlauncher.nix
+++ nix/modules/prismlauncher.nix
@@ -12,10 +12,17 @@ in
     homeManager.prismlauncher =
       { config, pkgs, ... }:
       {
-        xdg.desktopEntries.gtnh = {
-          name = "GregTech New Horizons";
-          icon = "${config.home.homeDirectory}/.local/share/PrismLauncher/instances/GT_New_Horizons_2.8.4_Java_17-25/icon.png";
-          exec = "prismlauncher --launch GT_New_Horizons_2.8.4_Java_17-25";
+        xdg.desktopEntries = {
+          gtnh = {
+            name = "GregTech New Horizons";
+            icon = "${config.home.homeDirectory}/.local/share/PrismLauncher/instances/GT_New_Horizons_2.8.4_Java_17-25/icon.png";
+            exec = "prismlauncher --launch GT_New_Horizons_2.8.4_Java_17-25";
+          };
+          gtnh-daily = {
+            name = "GT New Horizons (Daily)";
+            icon = "${config.home.homeDirectory}/.local/share/PrismLauncher/instances/GT New Horizons (Daily)/icon.png";
+            exec = "prismlauncher --launch \"GT New Horizons (Daily)\"";
+          };
         };
         home.packages = [
           (pkgs.prismlauncher.override {
