14d23d115deb53d776d0e193cd5142095c4a1b91
Mohammad Rafiq <rafiq@rrv.sh>
2026-06-14 13:00:23 +0800

    gtnh-server: wipe existing

--- nix/hosts.nix
+++ nix/hosts.nix
@@ -76,8 +76,6 @@ in
         cfg.modules.nixos.steam
         cfg.modules.nixos.prismlauncher
         cfg.modules.nixos.daily-midnight-poweroff
-        cfg.modules.nixos.gtnh-server
-        cfg.modules.nixos.gtnh-backups
         cfg.modules.nixos.gtnh-daily-server
         (
           {
